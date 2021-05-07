7628b0bb8deb9e41091d0876dea63747561dbf79
Rafael Liban <rafaelliban@gmail.com>
2021-05-07 09:59:12 +0800

    add release plugin

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
   id("org.springframework.boot") version "2.4.5"
   id("io.spring.dependency-management") version "1.0.11.RELEASE"
   id("com.google.cloud.tools.jib") version "3.0.0"
+  id("net.researchgate.release") version "2.8.1"
   id("java")
 }
 
@@ -40,4 +41,13 @@ jib {
     image = "docker.pkg.github.com/jrrl/ms3-contact-management-api/ms3-contact-api"
     tags = setOf("latest")
   }
+}
+
+release {
+  failOnCommitNeeded = false
+  failOnUnversionedFiles = false
+  failOnUpdateNeeded = false
+  preTagCommitMessage = "[skip ci] tag version: "
+  tagCommitMessage = "[skip ci] update to new version: "
+  newVersionCommitMessage = "[skip ci] new version commit: "
 }
